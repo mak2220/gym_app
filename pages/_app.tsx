98537cd0ea6a68a139974c9a6c0fd33e3a8722cb
mak2220 <bertamarcosesteban@gmail.com>
2024-11-12 09:40:17 -0300

    Incorporación de Footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "@/styles/globals.css";
 import "@/styles/normalize.css"
 import type { AppProps } from "next/app";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Navbar/>
       <Component {...pageProps} />
+      <Footer/>
     </>
   )
 }
